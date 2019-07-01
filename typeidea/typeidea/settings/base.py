e97ca5dbc96b4b1a7b94cad019ee3c9c371dd2f1
mariopq <quandonglei@126.com>
2019-07-01 22:03:19 +0800

    '完成view'

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'typeidea',
     'blog',
     'config',
     'comment',
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -1,25 +1,20 @@
-"""typeidea URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.conf.urls import url
+from django.contrib import admin
 
+from blog.views import (
+    IndexView, CategoryView, TagView,
+    PostDetailView,
+)
+from config.views import links
 from .custom_site import custom_site
 
 
 urlpatterns = [
-    url(r'^super_admin/', admin.site.urls),
-    url(r'^admin/', custom_site.urls),
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
+    url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
+    url(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post-detail'),
+    url(r'^links/$', links, name='links'),
+    url(r'^super_admin/', admin.site.urls, name='super-admin'),
+    url(r'^admin/', custom_site.urls, name='admin'),
 ]
